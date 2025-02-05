6075ed7f99ab062e44e2e35c98af7e385b2a03fa
claudiarawson <claudiarawson2021@gmail.com>
2025-02-05 14:55:16 -0800

    Function Set Up

---
+++ graph.py
@@ -0,0 +1,33 @@
+import argparse
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import math
+
+# Argument parser to handle the 
+def parse_arguments():
+    return
+
+# Load or create a graph
+
+# If input file is given, graph is loaded from .gml
+def load(file_path):
+    return
+
+# Otherwise, create a random Erdos-Renyi Graph
+def rcreate(n, c):
+    return
+
+# Find shortest path using Breadth First Search
+def bfs(G, start_node):
+    return
+
+# Using matplotlib to plot the graph and visualise the data
+def plot(G, bfs_paths = None):
+    return
+
+# Save graph into output file
+def save(G, output):
+    return
+
