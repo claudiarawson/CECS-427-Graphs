fad29d60210f0290bc26934fd877ff2309374836
claudiarawson <claudiarawson2021@gmail.com>
2025-02-05 17:27:33 -0800

    Load Function and Parse Argument Functions Complete

--- graph.py
+++ graph.py
@@ -1,16 +1,26 @@
 import argparse
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
 
-# Argument parser using argparse to handle the 
+# Argument parser using argparse to handle command line arguments
 def parse_arguments():
-    return
+    parser = argparse.ArgumentParser(descriptopn = "Erdos-Renyi Graph Generator and Analyser")
+    parser.add_argument("--input", type = str, help = "Input  graph file in .gml format")
+    parser.add_argument("--create_random_graph", nargs = 2, type = float, metavar = ("n", "c"), help = "Create an Erdos-Renyi random graph")
+    parser.add_argument("--BFS", type = str, help = "Compute shortest paths from a specified node using BFS")
+    parser.add_argument("--output", type = str, help = "Output file to save the graph in .gml format")
+
+    return parser.parse_args()
 
 # Load or create a graph
 
 # If input file is given, graph is loaded from .gml
 def load(file_path):
-    return
+    if not os.path.exists(file_path):
+        print(f"Error: File '{file_path}' does not exist.")
+        exit(1)
+    return nx.read_gml(file_path)
 
 # Otherwise, create a random Erdos-Renyi Graph
 def rcreate(n, c):
